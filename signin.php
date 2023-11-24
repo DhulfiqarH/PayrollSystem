63d5a20e172e702782167fcbd9e996a3f508adbf
DhulfiqarH <dhussei1@umbc.edu>
2023-11-23 21:36:36 -0500

    new commit

---
+++ signin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="signin.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+    <title>Sign In</title>
+</head>
+<body>
+  <?php
+  session_start();
+    include("navbar.php");
+  	include("sqlconnection.php");
+
+
+  	if($_SERVER['REQUEST_METHOD'] == "POST")
+  	{
+  		//something was posted
+  		$UserName = $_POST['UserName'];
+  		$Password = $_POST['Password'];
+
+  		if(!empty($UserName) && !empty($Password) && !empty($Email) && !empty($Phone) && !is_numeric($UserName))
+  		{
+  			//read from database
+  			$query = "select * from UserLogin where UserName = '$UserName' limit 1";
+  			$result = mysqli_query($con, $query);
+
+  			if($result)
+  			{
+  				if($result && mysqli_num_rows($result) > 0)
+  				{
+  					$user_data = mysqli_fetch_assoc($result);
+  				}
+  			}
+
+  			echo "wrong username or password!";
+  		}else
+  		{
+  			echo "wrong username or password!";
+  		}
+  	}
+
+  ?>
+    <div class="signin-container">
+        <h2>Sign In</h2>
+        <form action="signin.php" method="post">
+            <label for="username">Username:</label>
+            <input type="text" name="UserName" required>
+
+            <label for="password">Password:</label>
+            <input type="password" name="Password" required>
+
+            <button type="submit" name="signin">Sign In</button>
+        </form>
+    </div>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -4,26 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="signup.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
     <title>Sign Up</title>
 </head>
 <body>
-    <div class="signup-container">
-        <h2>Sign Up</h2>
-        <form action="signup_process.php" method="post">
-            <label for="email">Email:</label>
-            <input type="email" name="email" required>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="signup.css">
+      <link rel="stylesheet" type="text/css" href="navbar.css">
+      <title>Sign Up</title>
+  </head>
+  <body>
+      <?php include 'navbar.php'; ?>
+      <?php
+      session_start();
+      	include("sqlconnection.php");
 
-            <label for="phone">Phone:</label>
-            <input type="text" name="phone" required>
+      	if($_SERVER['REQUEST_METHOD'] == "POST")
+      	{
+      		//something was posted
+      		$UserName= $_POST['UserName'];
+      		$Password = $_POST['Password'];
+          $Email= $_POST['Email'];
+      		$Phone = $_POST['Password'];
 
-            <label for="username">Username:</label>
-            <input type="text" name="username" required>
+      		if(!empty($UserName) && !empty($Password) && !empty($Email) && !empty($Phone) && !is_numeric($UserName))
+      		{
+      			//save to database
+      			$query = "insert into users (UserName, Password, Email, Phone) values ('$UserName','$password', '$Email', '$Phone')";
+      			mysqli_query($con, $query);
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" required>
+      			header("Location: login.php");
+      			die;
+      		}else
+      		{
+      			echo "Please enter some valid information!";
+      		}
+      	}
+      ?>
+      <div class="signup-container">
+          <h2>Sign Up</h2>
+          <form action="signup.php" method="post">
+              <label for="Email">Email:</label>
+              <input type="Email" name="Email" required>
 
-            <button type="submit">Sign Up</button>
-        </form>
-    </div>
-</body>
-</html>
+              <label for="Phone">Phone:</label>
+              <input type="text" name="Phone" required>
+
+              <label for="UserName">Username:</label>
+              <input type="text" name="UserName" required>
+
+              <label for="Password">Password:</label>
+              <input type="Password" name="Password" required>
+
+              <button type="submit" name="reg_user">Sign Up</button>
+          </form>
+      </div>
+  </body>
+  </html>
