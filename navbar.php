aa437059e21a3cbae05a34eb17a9c2a5c20589c4
nissubba1 <nishansubbalim101@gmail.com>
2023-12-02 20:34:04 -0500

    added log out

--- navbar.php
+++ navbar.php
@@ -18,11 +18,14 @@
     <a href="deductionb.php" class="cool-button animated-button">Deduction List</a>
 </div>
 <div class="dropdown">
+          <a href="dropdown.php" class="cool-button animated-button">Log Out</a>
+    </div>
+<!-- <div class="dropdown">
     <span class="cool-button animated-button">Option</span>
     <div class="dropdown-content">
       <a href="signup.php" class="cool-button animated-button">Signup</a>
       <a href="signin.php" class="cool-button animated-button">Signin</a>
-    </div>
+    </div> -->
 </div>
 </body>
 </html>
