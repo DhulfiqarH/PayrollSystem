f70f82373ecfca5a391ed3d022b4f779e4dbbaa2
DhulfiqarH <dhussei1@umbc.edu>
2023-12-03 14:34:35 -0500

    update

--- navbar.php
+++ navbar.php
@@ -1,28 +1,34 @@
 <!-- navbar.php -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="navbar.css">
-  <link rel="stylesheet" type="text/css" href="dropdown.css">
-  <title>admin dash</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+    <link rel="stylesheet" type="text/css" href="dropdown.css">
+    <title>admin dash</title>
 </head>
+
 <body>
-<div class="Main-buttons">
-    <a href="admindash.php" class="cool-button animated-button">Home</a>
-    <a href="attentance.php" class="cool-button animated-button">Attendance</a>
-    <a href="payroll.php" class="cool-button animated-button">Payroll</a>
-    <a href="employee.php" class="cool-button animated-button">Employee list</a>
-    <a href="department.php" class="cool-button animated-button">Department</a>
-    <a href="deductionb.php" class="cool-button animated-button">Deduction List</a>
-</div>
-<div class="dropdown">
-          <a href="dropdown.php" class="cool-button animated-button">Log Out</a>
-</div>
-  <!-- <div class="name">
+    <div class="Main-buttons">
+        <a href="admindash.php" class="cool-button animated-button">Home</a>
+        <a href="attentance.php" class="cool-button animated-button">Attendance</a>
+        <a href="payroll.php" class="cool-button animated-button">Payroll</a>
+        <a href="employee.php" class="cool-button animated-button">Employee List</a>
+        <a href="department.php" class="cool-button animated-button">Department</a>
+        <a href="deductionb.php" class="cool-button animated-button">Deduction List</a>
+    </div>
+    <div class="dropdown">
+        <a href="dropdown.php" class="cool-button animated-button">Log Out</a>
+    </div>
+    <!-- <div class="name">
           <p class="name-cool">Hello Admin</p>
     </div> -->
+    <div class="name">
+        <p class="name-cool animated-button">Hello Admin</p>
+    </div>
 
 </body>
+
 </html>
