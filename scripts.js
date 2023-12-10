12c64f9c192cc6b0c6d261cb3a9c7f84fa3beab9
DhulfiqarH <dhussei1@umbc.edu>
2023-12-10 13:03:21 -0500

    graph updated, client dash updated

--- scripts.js
+++ scripts.js
@@ -15,3 +15,33 @@ window.addEventListener('scroll', function () {
     // Apply the opacity to the video
     video.style.opacity = opacity;
 });
+
+<script>
+    // Assuming $result contains the query result
+    var payrollData = <?php echo json_encode($payrollData); ?>;
+
+    // Extract dates and amounts from the PHP result
+    var dates = payrollData.map(item => item.DateProcessed);
+    var amounts = payrollData.map(item => item.NetIncome);
+
+    // Convert dates to a more readable format (you might want to customize this)
+    var formattedDates = dates.map(date => new Date(date).toLocaleDateString());
+
+    // Get the canvas element
+    var ctx = document.getElementById('payrollChart').getContext('2d');
+
+    // Create the chart
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: formattedDates,
+            datasets: [{
+                label: 'Net Income ($)',
+                data: amounts,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        }
+    });
+</script>
